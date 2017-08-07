3ec36411002c8e6f19dce77d784567bce4d776a4
Eric King <Eric King>
2017-08-06 21:51:29 -0400

    added logos to assets, created create project modal/form

--- src/components/Projects/CreateProject.js
+++ src/components/Projects/CreateProject.js
@@ -1,26 +1,44 @@
 import React from 'react'
+import {Modal, Button, Input, Row} from 'react-materialize'
 
 const CreateProject = () => (
-  <div className='row'>
-    <form className='col s12'>
-      <div className='row'>
-        <div className='input-field col s6'>
-          <input placeholder='Project Name' id='projectName' type='text' className='validate' />
-        </div>
-        <div className='row'>
-          <div className='input-field col s6'>
-            <textarea placeholder='Description'id='description' className='materialize-textarea' />
-          </div>
-          <div className='input-field col s6'>
-            <input placeholder='Number of Collaborators' id='numCollab' type='text' className='validate' />
-          </div>
-        </div>
-        <div className='row'>
-          <a className='waves-effect waves-light btn' type='submit' name='action'>Create Project</a>
-        </div>
-      </div>
-    </form>
-  </div>
+  <Modal
+    header='Create New Project'
+    trigger={<Button style={styles.center}>Create Project</Button>}>
+    <Row>
+      <Input s={12} label='Project Name' />
+      <Input s={12} label='Short Description' />
+    </Row>
+    <Row>
+      <Input s={12} type='select' label='Number of Collaborators Wanted' defaultValue='2'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+      </Input>
+    </Row>
+    <h2 Main Language />
+    <Row>
+      <Input name='group1' type='checkbox' value='JavaScript' label='JavaScript' />
+      <Input name='group1' type='checkbox' value='Ruby' label='Ruby' />
+      <Input name='group1' type='checkbox' value='Python' label='Python' />
+      <Input name='group1' type='checkbox' value='C/C#/C++' label='C/C#/C++' />
+      <Input name='group1' type='checkbox' value='Java' label='Java' />
+      <Input name='group1' type='checkbox' value='PHP' label='PHP' />
+    </Row>
+    <Row>
+      <Button waves='light' style={styles.button}>Submit</Button>
+    </Row>
+  </Modal>
 )
 
 export default CreateProject
+
+const styles = {
+  button: {
+    backgroundColor: '#6ece5a',
+    margin: '10px',
+    fontFamily: 'Share Tech Mono'
+  }
+}
--- src/components/Recurrent/Navbar.js
+++ src/components/Recurrent/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => (
-  <nav className='navbar-fixed  z-depth-0' style={navStyle}>
+  <nav className='navbar-fixed  z-depth-0' style={styles.navStyles}>
     <div className='nav-wrapper'>
       <Link to='/' className='brand-logo' style={{paddingLeft: 15}}>{'<ST />'}</Link>
       <ul id='nav-mobile' className='right hide-on-med-and-down'>
@@ -13,9 +13,11 @@ const Navbar = () => (
   </nav>
 )
 
-const navStyle = {
-  fontFamily: 'Share Tech Mono',
-  backgroundColor: '#172b4c'
+const styles = {
+  navStyles: {
+    fontFamily: 'Share Tech Mono',
+    backgroundColor: '#172b4c'
+  }
 }
 
 export default Navbar
